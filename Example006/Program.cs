21a3b8f68ab5a69bf9169e426927266a4745b8e0
KACTPO93 <ibra_uf@mail.ru>
2023-02-11 23:29:20 +0300

    Example006 6 задача

---
+++ Example006/Program.cs
@@ -0,0 +1,12 @@
+﻿//Программа которая на вход принимает число и выдаёт, является ли число чётным
+//(делится ли оно на два без остатка).
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int sqr = a;
+if (sqr % 2 ==0)
+    {
+    Console.WriteLine("Четное!");
+    }
+        else
+        {Console.WriteLine("Нечётное!");
+        }
