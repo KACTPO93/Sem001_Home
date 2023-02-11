23253662035e1d099d48a006305769c5da968af2
KACTPO93 <ibra_uf@mail.ru>
2023-02-11 23:00:31 +0300

    Example004 4 задача

--- Example004/Program.cs
+++ Example004/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите первое число: ");
+﻿//Принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое число: ");
 int a = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int b = int.Parse(Console.ReadLine());
