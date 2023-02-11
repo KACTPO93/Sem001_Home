6c40a7e72f3ee30d2bc202d2c8d811b3cee49b3d
KACTPO93 <ibra_uf@mail.ru>
2023-02-11 22:44:53 +0300

    Example002

---
+++ Example002/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт,
+//какое число большее, а какое меньшее.
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int max = a;
+int min = a;
+if (b > max) max = b;
+Console.Write("min = ");
+Console.WriteLine(min);
+Console.Write("max = ");
+Console.WriteLine(max);
