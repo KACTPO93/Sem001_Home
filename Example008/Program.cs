de3a681a2ae6d223a4dff9af2d8ebe8a66d3023f
KACTPO93 <ibra_uf@mail.ru>
2023-02-12 00:17:32 +0300

    Example008 8  задача, ничего не понял)

---
+++ Example008/Program.cs
@@ -0,0 +1,13 @@
+﻿//
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+while(count<N)
+{
+    if (N % 2 ==0)
+    {
+    Console.WriteLine("N!");
+    count = count + 1;
+    }
+     
+}
